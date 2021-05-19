7f0d46488dd649475693375dfa98b0568c32fabd
SIAGRI\mauricio.oliveira <mauricio.oliveira@siagri.com.br>
2021-05-19 15:54:16 -0300

    Primeiro Model View Controller - Departmets

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Controllers
 {
