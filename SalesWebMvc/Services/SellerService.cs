685beaee1dac7d637c736acab32506ce2f35d9f5
SIAGRI\mauricio.oliveira <mauricio.oliveira@siagri.com.br>
2021-05-24 10:27:35 -0300

    Edicao vendedor e excecoes de servico

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
+
 namespace SalesWebMvc.Services
 {
     public class SellerService
@@ -13,19 +15,16 @@ namespace SalesWebMvc.Services
         {
             _context = context;
         }
-
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
         }
-
         public void Insert(Seller obj)
         {
             //obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
-
         public Seller FindById(int id)
         {
             //Traz apenas o ID do departamento
@@ -33,12 +32,27 @@ namespace SalesWebMvc.Services
             //Traz o Id e o restante dos dados do departamento
             return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
-
         public void remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(s => s.Id == obj.Id))
+            {
+                throw new NotFoundException("Seller´s Id not Found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new DbConcurrencyException(ex.Message);
+            }
         }
     }
 }
