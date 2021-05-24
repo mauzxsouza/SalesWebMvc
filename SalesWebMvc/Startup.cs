162cb5491b23fd9044503f99934651267c2def8b
SIAGRI\mauricio.oliveira <mauricio.oliveira@siagri.com.br>
2021-05-24 11:51:20 -0300

    Localizacao e formatacao de numeros e datas

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
 
@@ -49,6 +49,16 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var emUSA = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(emUSA),
+                SupportedCultures = new List<CultureInfo> { emUSA },
+                SupportedUICultures = new List<CultureInfo> { emUSA }
+            };
+
+            app.UseRequestLocalization(localizationOptions);                
+
             if (env.IsDevelopment())
             {
                 seedingService.Seed();
